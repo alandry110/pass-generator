248b129b4a49bcc8202e6b4cd4904cc89ca26dbc
Ashlynn Landry <ashclandry@gmail.com>
2022-07-23 18:37:31 -0400

    trouble shooting method for undefined variable worked! Application is functioning properly.

--- README.md
+++ README.md
@@ -1,10 +1,46 @@
 # JavaScript Challenge: Password Generator 
 
 ## About the Repository
-
 This client has access to sensitive data and would like for me to create a password generator that uses a series of prompts that leads to a strong password that will provide the client with greater security. 
 
 ## Acceptance Criteria
 For this project, I was given a set of critical requirements to satisfy the clients password needs. They are as follows:
 
 ![Screenshot of acceptance criteria](./assets/images/Screen%20Shot%202022-07-21%20at%209.58.58%20PM.png)
+
+## Additional instruction to complete the final challenge included:
+* Application deploys at live URL with no loading errors 
+* Should not produce any errors in the console when you inspect it using Chrome DevTools.
+* Application user experience is intuitive and easy to navigate
+* Application user interface style is clean and polished
+* Application resembles the mock-up functionality in acceptance criteria 
+* Repository as a unique name, has proper file structure, contains multiple descriptive commit messages, and a quality README file with description, screenshot of the final project, and a link to the live deployed application
+
+## Description and Credits 
+
+To make my JavaScript meet the acceptance criteria, I sought help from multiple resources, which include:
+
+* w3schools: I used w3schools to help me understand loops and conditions better.
+* YouTube by Paul Keldsen - Web Development(https://www.youtube.com/watch?v=x4HUaiazDes&start=160): This video helped me understand the importance of Pseudocoding and help me grasped where to begin. 
+* MDN Links from the JavaScript mini project: 
+    - https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+    - https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm 
+    - https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
+    - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math 
+* I drew inspiration from a multiple activities we did during one of our lectures on JavaScript.
+* I collaborated with my study group from class on loops & debugging.
+
+README.md file:
+* I used the following link to help me with the layout for this README file:
+https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+
+## Final submission links:
+
+* GitHub Repository - https://github.com/alandry110/pass-generator
+* GitHub Deployed Page - https://alandry110.github.io/my-portfolio/
+
+
+## Heres a video of how my password generator functions:
+
+![Video of Functioning Password Generator](./)
+
--- assets/script.js
+++ assets/script.js
@@ -18,53 +18,61 @@ var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var symbols = ["!","@","#","$","%","^","&","•","?"];
 var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
+var charLength = "";
+var selection = [];
+var userPass = "";
 
 function generatePassword() {
-  var selection = [];
-  var finalPass = "";
-  // console.log("Button is Working!")
-    var askQuestions = window.confirm("Click OK for questions that will lead you to a secure password.");
+    var start = window.confirm("Click OK for questions that will lead you to a secure password.");
       // If user clicks 'cancel' they will be prompted to start over
-      if (!askQuestions) {
+      if (!start) {
         alert("No password for you!");
-  
-        return "press 'Generate Password' to begin again.";
+        return "Press 'Generate Password' to begin again.";
       }
+
     var charLength = window.prompt("How many characters would you like your password to contain?");
       // If a number is chosen less than 8 or greater than 128 user will be prompted to start over
-      if (charLength <8 || charLength >128) {
-        alert("must chose a number between 8 and 128");
-    
-        return "press 'Generate Password' to begin again.";
+      if (charLength < 8 || charLength > 128) {
+        alert("Must chose a number between 8 and 128");
+        return "Press 'Generate Password' to begin again.";
       }
+
     var specialChar = window.confirm("Click OK to include special characters.");
-      if (specialChar) {
-        selection.push(symbols);
-      }
     var numericChar = window.confirm("Click OK to include numeric characters.");
-      if (numericChar) {
-        selection.push(numbers);
-      }
     var lowerChar = window.confirm("Click OK to include lowercase letters.");
-      if (lowerChar) {
-        selection.push(lowercase);
-      }
     var upperChar = window.confirm("Click OK to include uppercase letters.");
-      if (upperChar) {
-        selection.push(uppercase);
-      }
-  
-    for (var i = 0; i < password.length; i++) {
-      finalPass += selection[Math.floor(Math.random() + selection.length)];
-    
 
-      return finalPass
+    if (!specialChar && !numericChar && !lowerChar && !upperChar) {
+      alert("You need to pick at least one!")
+      return "Error! Press 'Generate Password' to begin again.";
     }
-   
+    
+    if (numericChar) {
+      selection = selection.concat(numbers);
+    }
 
-  //return "Password will go here!";// Used to see where the password will be displayed.
+    if (specialChar) {
+      selection = selection.concat(symbols);
+    }
+
+    if (lowerChar) {
+      selection = selection.concat(lowercase);
+    }
+    
+    if (upperChar) {
+      selection = selection.concat(uppercase);
+    }
+    
+    console.log(selection)
+      
+    for (var i = 0; i < charLength; i++) {
+      userPass = userPass + selection[Math.floor(Math.random() * selection.length)];
+      console.log(userPass)
+    }
+    
+    return userPass;    
 }
+   
 
 // Write password to the #password input
 function writePassword() {
